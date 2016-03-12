6a7520fcc42007f74a69a365ca9b525be103c472
Coby Randquist <randquistcp@gmail.com>
2016-03-11 20:28:42 -0800

    changes to refactor chart building out of the controller into a helper

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,43 +6,7 @@ class DashboardsController < ApplicationController
     @widget_charts = {}
 
     @widgets.each do |widget|
-      fds = widget.data_sources.first
-      m_count = widget.data_sources.map {|ds| ds.measurement }.uniq.count
-      categories = fds.time_series.reverse.map {|p| Time.zone.parse(p[0]).strftime("%Y-%m-%d %I:%M:%S %P")}
-
-      chart = LazyHighCharts::HighChart.new('graph') do |f|
-        #f.xAxis(type: 'datetime',
-        #        dateTimeLabelFormats: {
-        #          month: '%e %b',
-        #          day: '%e' },
-        f.xAxis(categories: categories,
-                crosshair: true)
-        if m_count == 2
-          f.yAxis [
-                   { labels: { format: "{value}#{fds.symbol}" },
-                     title:  { text: 'Temperature' },
-                     min: 40},
-                   { labels: { format: "{value}%" },
-                     title:  { text: 'Humidity' },
-                     min: 40,
-                     opposite: true }
-                  ]
-        else
-          f.yAxis( labels: { format: "{value}#{fds.symbol}" },
-                   title:  { text: 'Temperature' } )
-        end
-        f.legend(verticalAlign: 'top', align: 'center', layout: 'horizontal')
-
-        widget.data_sources.each do |ds|
-          f.series(
-                   type: 'spline',
-                   name: "#{ds.device.location} #{ds.symbol}",
-                   data: ds.time_series.reverse.map {|d| d[1]},
-                   yAxis: "#{ds.measurement == 'temperature' ? 0 : 1}".to_i
-                   )
-        end
-      end
-      @widget_charts[widget.id] = chart
+      @widget_charts[widget.id] = build_chart widget
     end
   end
 end
--- app/helpers/widgets_helper.rb
+++ app/helpers/widgets_helper.rb
@@ -9,4 +9,42 @@ module WidgetsHelper
     f_symbol = content_tag(:span,data_source.symbol,class: 'symbol')
     string_value = "%.#{data_source.precision || 0}f" % value || 0
   end
+
+  def build_cart widget
+    fds = widget.data_sources.first
+    m_count = widget.data_sources.map {|ds| ds.measurement }.uniq.count
+    categories = fds.time_series.reverse.map {|p| Time.zone.parse(p[0]).strftime("%Y-%m-%d %I:%M:%S %P")}
+
+    chart = LazyHighCharts::HighChart.new('graph') do |f|
+      f.xAxis(categories: categories,
+              crosshair: true)
+
+      if m_count == 2
+        f.yAxis [
+                 { labels: { format: "{value}#{fds.symbol}" },
+                   title:  { text: 'Temperature' },
+                   min: 40},
+                 { labels: { format: "{value}%" },
+                   title:  { text: 'Humidity' },
+                   min: 40,
+                   opposite: true }
+                ]
+      else
+        f.yAxis( labels: { format: "{value}#{fds.symbol}" },
+                 title:  { text: 'Temperature' } )
+      end
+      f.legend(verticalAlign: 'top', align: 'center', layout: 'horizontal')
+
+      widget.data_sources.each do |ds|
+        f.series(
+                 type: 'spline',
+                 name: "#{ds.device.location} #{ds.symbol}",
+                 data: ds.time_series.reverse.map {|d| d[1]},
+                 yAxis: "#{ds.measurement == 'temperature' ? 0 : 1}".to_i
+                 )
+      end
+    end
+
+    return chart
+  end
 end
