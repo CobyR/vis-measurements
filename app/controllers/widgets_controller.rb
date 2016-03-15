e4a1195b2c8d69e4212a383fe83e7d2a7cee15ab
Coby Randquist <randquistcp@gmail.com>
2016-03-15 00:57:08 -0700

    securing things a bit more

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -71,7 +71,7 @@ class WidgetsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_widget
-      @widget = Widget.find(params[:id])
+      @widget = current_user.widgets.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
